1bb836e4eae5c22a1f642dc4b620cc6a7613c2be
gumelar.wicaksono6 <gumelar.wicaksono6@gmail.com>
2025-01-13 15:32:54 +0000

    update tomol views

--- functions/app.js
+++ functions/app.js
@@ -5,8 +5,8 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')))
-app.set('views', path.join(__dirname, './views'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
